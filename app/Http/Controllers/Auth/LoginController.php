3717ac3aad4985f33a6389bf87d4fc275d0ff0c3
Anum1212 <anamamer0@gmail.com>
2018-02-13 02:00:07 +0500

    overriding default auth methods -> credentials method to check for verificationStatus -> sendFailedLoginResponse to show error if login fails because account is not verified

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Auth;
+use App\User;
 
 class LoginController extends Controller
 {
@@ -37,6 +39,35 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout','userlogout');
     }
+
+// overriding default credentials method to check verificationStatus of user
+    protected function credentials(Request $request)
+    {
+        return ['email'=>$request->{$this->username()},'password' => $request->password, 'verificationStatus' => '1'];
+    }
+
+// overriding default sendFailedLoginResponse method to show appropriate error message to user if verificationStatus is 0 -> email not verified
+    protected function sendFailedLoginResponse(Request $request)
+    {
+        $errors = [$this->username() => trans('auth.failed')];
+
+        // Load user from database
+        $user = User::where($this->username(), $request->{$this->username()})->first();
+
+        // Check if user was successfully loaded, that the password matches
+        // and verificationStatus is 0. If so, override the default error message.
+        if ($user && \Hash::check($request->password, $user->password) && $user->verificationStatus == 0) {
+          return redirect()->back()->withInput($request->only('email','remember'))->with('error', 'Verify Account First');
+        }
+
+        if ($request->expectsJson()) {
+            return response()->json($errors, 422);
+        }
+        return redirect()->back()
+            ->withInput($request->only($this->username(), 'remember'))
+            ->withErrors($errors);
+    }
+
 
     public function userlogout()
     {
