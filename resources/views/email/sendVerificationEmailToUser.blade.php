c34e8b3eeea19d8b5d37a643b46a90b4e9e70344
Anum1212 <anamamer0@gmail.com>
2018-02-13 14:57:23 +0500

    added a little css (will further imporove later)

--- resources/views/email/sendVerificationEmailToUser.blade.php
+++ resources/views/email/sendVerificationEmailToUser.blade.php
@@ -1,2 +1,29 @@
-to verify email
-  <a href="{{route('sendEmailToUser', ["email" => $user->email, "verificationToken" => $user->verificationToken])}}">Click Here</a>
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+  #header{
+    background: #BD1616;
+    color: white;
+    text-align: center;
+  }
+  </style>
+    </head>
+    <body>
+  <div id="emailBody">
+      <div id="header">
+        <h1>Pharmacy</h1>
+      </div>
+      <div id="content">
+        A.S.A {{$user->name}},<br />
+        <p>
+          to verify email
+          <a href="{{route('sendEmailToUser', ["email" => $user->email, "verificationToken" => $user->verificationToken])}}">Click Here</a>
+        </p>
+      </div>
+    </div>
+    </body>
+  </html>
