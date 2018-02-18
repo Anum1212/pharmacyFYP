4522dc3ffcd6cd8833b47fdc5737d99bd5e1c2a0
Anum1212 <anamamer0@gmail.com>
2018-02-18 12:39:27 +0500

    removed status setting. becuase now status will be set by default. added more code in sendVerifyMethod so that when pharmacist varifies email a record for that pharmacy is created in ratings table with 0 rating

--- app/Http/Controllers/Auth/PharmacistRegisterController.php
+++ app/Http/Controllers/Auth/PharmacistRegisterController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Str;
 use App\Pharmacist;
+use App\Rating;
 use Auth;
 use Geocode;
 use Mail;
@@ -49,7 +50,7 @@ class PharmacistRegisterController extends Controller
       // validate the data
       $this->validate($req, [
         'name'          =>      'required|string|max:255',
-        'email'         =>      'required|string|email|max:255|unique:users',
+        'email'         =>      'required|string|email|max:255|unique:pharmacists',
         'contact'       =>      'required|digits:11',
         'pharmacyName'  =>      'required',
         'address'       =>      'required',
@@ -66,7 +67,6 @@ class PharmacistRegisterController extends Controller
     	$longitude = $addressToLatLng->longitude();
       // store in the database
       $pharmacists = new Pharmacist;
-      $pharmacists->status = '0';
       $pharmacists->name = $req->name;
       $pharmacists->email = $req->email;
       $pharmacists->contact = $req->contact;
@@ -78,12 +78,13 @@ class PharmacistRegisterController extends Controller
       $pharmacists->longitude = $longitude;
       $pharmacists->password=bcrypt($req->password);
       $pharmacists->verificationToken=Str::random(40);
+      // by default verificationStatus and status is set to 0 and 1 respectively
+      // see create_pharmacists_table migration for more info
       $pharmacists->save();
 
       $thisUser = $pharmacists;
       $this->sendEmail($thisUser);
-//
-// return redirect(route('verifyEmailFirst'));
+
       return redirect()->route('pharmacist.login')->with('message', 'A confirmation email has been sent');
 }
   }
@@ -100,6 +101,11 @@ class PharmacistRegisterController extends Controller
     $pharmacist = Pharmacist::where(['email' => $email, 'verificationToken' => $verificationToken])->first();
     if($pharmacist){
       Pharmacist::where(['email' => $email, 'verificationToken' => $verificationToken])->update(['verificationStatus'=>'1', 'verificationToken' => NULL]);
+      $defaultPharmacyRating = new Rating;
+      $defaultPharmacyRating->pharmacyId = $pharmacist->id;
+      $defaultPharmacyRating->pharmacyName = $pharmacist->pharmacyName;
+      $defaultPharmacyRating->save();
+
       return redirect()->route('pharmacist.login')->with('message', 'Verification Successful');
     }
     else {
