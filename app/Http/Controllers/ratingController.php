f7ad62a1c1a56ac68bed0229dd2a8d79a48ece7e
Anum1212 <anamamer0@gmail.com>
2018-02-18 12:41:33 +0500

    rating controller to view and save ratings of pharmacy.

---
+++ app/Http/Controllers/ratingController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Rating;
+use App\Pharmacist;
+
+class ratingController extends Controller
+{
+
+  public function __construct()
+  {
+      $this->middleware('auth');
+  }
+
+  public function index()
+  {
+    // getting activated pahrmacy records
+    $pharmacies = Pharmacist::where([
+            ['status', '=', '1'], // Pharmacy is not banned by admin
+                ['verificationStatus', '=', '1'], // pharmacist has verified the email
+                ])->get();
+    // getting ratings of those "$pharmacies" form ratings table
+    // we can directly read the ratings table but this way will only get result of pharmacies that are enabled
+    for($i=0; $i<count($pharmacies);$i++)
+    $pharmacyRatings[] = Rating::where('pharmacyId', $pharmacies[$i]->id)->first();
+    return view('ratingsPage', compact('pharmacyRatings'));
+  }
+
+
+    public function ratePharmacy(Request $req, $pharmacyId)
+    {
+      echo $req->rating;
+      $currentRating = Rating::where('pharmacyId', $pharmacyId)->first();
+
+      // to convert the average back to total points
+      $totalRating = $currentRating->rating*$currentRating->noOfUserThatRated;
+      //adding 1 to old noOfUserThatRated to get new noOfUserThatRated
+      $currentRating->noOfUserThatRated = $currentRating->noOfUserThatRated+1;
+      // average formula (old total points + new user points)/ total number of people that rated
+      $currentRating->rating = ($totalRating+$req->rating)/$currentRating->noOfUserThatRated;
+
+      // update rating
+      $currentRating->update();
+      return redirect()->action('ratingController@index');
+    }
+}
