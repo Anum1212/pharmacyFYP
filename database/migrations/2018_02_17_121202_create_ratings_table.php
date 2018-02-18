ed57000a1d15c0366743bcb0d44894ba8ab569a4
Anum1212 <anamamer0@gmail.com>
2018-02-18 12:42:03 +0500

    rating migration table

---
+++ database/migrations/2018_02_17_121202_create_ratings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRatingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ratings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('pharmacyId');
+            $table->string('pharmacyName');
+            $table->float('rating', 5, 1)->default('0.0');
+            $table->integer('noOfUserThatRated')->default('0');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ratings');
+    }
+}
